8dc28e920e416f2d3b60bbf695c54628858fb754
JohnNy_cz <jan.raszyk@gmail.com>
2014-04-23 21:06:57 +0200

    initial commit, last known state from qw-dev.net

---
+++ tests/index.php
@@ -0,0 +1,50 @@
+<ul>
+	<li><ul>
+		<li><a href='../logs/- darkstorm - vs - Dar - [dmm3] - [dm6]_000.log'>- darkstorm - vs - Dar - [dmm3] - [dm6]_000.log</a>
+		<li><a href='../logs/- darkstorm - vs - Dar - [dmm3] - [dm6]_000.log.parsed'>- darkstorm - vs - Dar - [dmm3] - [dm6]_000.log.parsed</a>
+		<li><a href='../logs/- darkstorm - vs - Dar - [dmm3] - [dm6]_000.log.html'>- darkstorm - vs - Dar - [dmm3] - [dm6]_000.log.html</a>
+		<li><a href='../logs/- darkstorm - vs - Dar - [dmm3] - [dm6]_000.log.xml'>- darkstorm - vs - Dar - [dmm3] - [dm6]_000.log.xml</a>
+		<li><a href='../logs/- darkstorm - vs - Dar - [dmm3] - [dm6]_000.log.json'>- darkstorm - vs - Dar - [dmm3] - [dm6]_000.log.json</a>
+	</ul>
+
+	<li><ul>
+		<li><a href='../logs/11-01-06_pleuraxeraphim_vs_n_x__povdmm4__000.log'>11-01-06_pleuraxeraphim_vs_n_x__povdmm4__000.log</a>
+		<li><a href='../logs/11-01-06_pleuraxeraphim_vs_n_x__povdmm4__000.log.parsed'>11-01-06_pleuraxeraphim_vs_n_x__povdmm4__000.log.parsed</a>
+		<li><a href='../logs/11-01-06_pleuraxeraphim_vs_n_x__povdmm4__000.log.html'>11-01-06_pleuraxeraphim_vs_n_x__povdmm4__000.log.html</a>
+		<li><a href='../logs/11-01-06_pleuraxeraphim_vs_n_x__povdmm4__000.log.xml'>11-01-06_pleuraxeraphim_vs_n_x__povdmm4__000.log.xml</a>
+		<li><a href='../logs/11-01-06_pleuraxeraphim_vs_n_x__povdmm4__000.log.json'>11-01-06_pleuraxeraphim_vs_n_x__povdmm4__000.log.json</a>
+	</ul>
+
+	<li><ul>
+		<li><a href='../logs/a.log'>a.log</a>
+		<li><a href='../logs/a.log.parsed'>a.log.parsed</a>
+		<li><a href='../logs/a.log.html'>a.log.html</a>
+		<li><a href='../logs/a.log.xml'>a.log.xml</a>
+		<li><a href='../logs/a.log.json'>a.log.json</a>
+	</ul>
+
+	<li><ul>
+		<li><a href='../logs/b.log'>b.log</a>
+		<li><a href='../logs/b.log.parsed'>b.log.parsed</a>
+		<li><a href='../logs/b.log.html'>b.log.html</a>
+		<li><a href='../logs/b.log.xml'>b.log.xml</a>
+		<li><a href='../logs/b.log.json'>b.log.json</a>
+	</ul>
+
+	<li><ul>
+		<li><a href='../logs/johnny_cz - [johnny_cz_&_[MozarT]_vs_[.FraG.]_&_Shp3Q] - [dm2]_000.log'>johnny_cz - [johnny_cz_&_[MozarT]_vs_[.FraG.]_&_Shp3Q] - [dm2]_000.log</a>
+		<li><a href='../logs/johnny_cz - [johnny_cz_&_[MozarT]_vs_[.FraG.]_&_Shp3Q] - [dm2]_000.log.parsed'>johnny_cz - [johnny_cz_&_[MozarT]_vs_[.FraG.]_&_Shp3Q] - [dm2]_000.log.parsed</a>
+		<li><a href='../logs/johnny_cz - [johnny_cz_&_[MozarT]_vs_[.FraG.]_&_Shp3Q] - [dm2]_000.log.html'>johnny_cz - [johnny_cz_&_[MozarT]_vs_[.FraG.]_&_Shp3Q] - [dm2]_000.log.html</a>
+		<li><a href='../logs/johnny_cz - [johnny_cz_&_[MozarT]_vs_[.FraG.]_&_Shp3Q] - [dm2]_000.log.xml'>johnny_cz - [johnny_cz_&_[MozarT]_vs_[.FraG.]_&_Shp3Q] - [dm2]_000.log.xml</a>
+		<li><a href='../logs/johnny_cz - [johnny_cz_&_[MozarT]_vs_[.FraG.]_&_Shp3Q] - [dm2]_000.log.json'>johnny_cz - [johnny_cz_&_[MozarT]_vs_[.FraG.]_&_Shp3Q] - [dm2]_000.log.json</a>
+	</ul>
+
+	<li><ul>
+		<li><a href='../logs/ojoj.log'>ojoj.log</a>
+		<li><a href='../logs/ojoj.log.parsed'>ojoj.log.parsed</a>
+		<li><a href='../logs/ojoj.log.html'>ojoj.log.html</a>
+		<li><a href='../logs/ojoj.log.xml'>ojoj.log.xml</a>
+		<li><a href='../logs/ojoj.log.json'>ojoj.log.json</a>
+	</ul>
+
+</ul>
